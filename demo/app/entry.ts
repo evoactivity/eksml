37a91b5278c982e9bd7efd51fe8a14a6547ac94e
Liam Potter <liam@liampotter.co.uk>
2026-04-05 01:03:39 +0100

    lint fix

--- demo/app/entry.ts
+++ demo/app/entry.ts
@@ -1,11 +1,12 @@
+import { shouldRehydrate } from 'vite-ember-ssr/client';
+
 import Application from './app.ts';
 import config from './config.ts';
-import { shouldRehydrate } from 'vite-ember-ssr/client';
 
 if (shouldRehydrate()) {
   const app = Application.create({ ...config.APP, autoboot: false });
 
-  app.visit(window.location.pathname + window.location.search, {
+  void app.visit(window.location.pathname + window.location.search, {
     _renderMode: 'rehydrate',
   });
 } else {
